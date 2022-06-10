3fb01c0c02eabd68c9697ee63bced148e3b65501
sharoonsaxena <charismaticsharoon@gmail.com>
2022-06-11 00:02:14 +0530

    added deletion

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -52,14 +52,16 @@ class SingleLinkedList:
                 current_node.next = new_node
 
     def traverse(self):
-        """Traverses and prints the contents of linked list"""
+        """Traverses and prints the contents of linked list."""
         if self.head is None:
             print("Single Linked List is Empty!")
         else:
+            print("head")
             current_node = self.head
             while current_node is not None:
                 print(current_node.data)
                 current_node = current_node.next
+            print("tail")
 
     def find_data(self, data):
         """Finds a data value in the link list
@@ -78,20 +80,62 @@ class SingleLinkedList:
                 return current_node.data
             current_node = current_node.next
         return "Element not found in Singly Linked List"
-    
-    def delete_node(self, location):
-        
-        
 
-    
+    def delete_node(self, location):
+        """deletes node from linked list
+
+        Args:
+            location (int): location of node to delete
+
+        Returns:
+            string: message
+        """
+        if self.head is None:  # No Node
+            return "Singly Linked List is empty!"
+        else:
+            if location == 0:  # check empty
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head == self.head.next  # delete first node
+            elif location == 1:  # delete last
+                if self.head == self.tail:  # check empty
+                    self.head = None
+                    self.tail = None
+                else:
+                    current_node = self.head
+                    while current_node is not None:
+                        if current_node.next == self.tail:
+                            break
+                        current_node = current_node.next
+                    current_node.next = None
+                    self.tail = current_node
+            else:
+                current_node = self.head
+                index = 0
+                while index < location - 1:
+                    current_node = current_node.next
+                    index += 1
+                current_node = current_node.next.next
+
+
 linklist = SingleLinkedList()
+linklist.traverse()
+linklist.delete_node(0)
+linklist.traverse()
 linklist.insert_node(1, 1)
+linklist.delete_node(0)
 linklist.insert_node(2, 1)
 linklist.insert_node(3, 1)
+linklist.delete_node(1)
+linklist.traverse()
+linklist.insert_node(4, 1)
+linklist.insert_node(4, 1)
+linklist.insert_node(4, 1)
 linklist.insert_node(4, 1)
 linklist.insert_node(0, 0)
 linklist.insert_node(100, 0)
 
-# linklist.traverse()
-
-print(linklist.find_data(33))
+linklist.traverse()
+# print(linklist.find_data(33))
